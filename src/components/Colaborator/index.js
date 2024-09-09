deccf923f0b50237e4a784be83d0ff015eb06fe8
MaCintr <marcelocintra2307@gmail.com>
2024-09-09 12:00:51 -0300

    Criação de botão de deletar card (ainda não funcional)

--- src/components/Colaborator/index.js
+++ src/components/Colaborator/index.js
@@ -2,17 +2,19 @@ import './Colaborator.css'
 
 const Colaborator = (props) => {
     return (
-    <div className='colaborator' style={{borderColor: props.cor}}>
-        <div className='header'  style={{backgroundColor: props.cor}}>
-            <img src={props.img} alt={props.nome}/>
+        <div className='colaborator' style={{ borderColor: props.cor }}>
+            <div className='header' style={{ backgroundColor: props.cor }}>
+                <img src={props.img} alt={props.nome} />
+                <button className="delete-button">
+                    <i className="bi bi-trash3"></i>
+                </button>
+            </div>
+            <div className='base'>
+                <h4>{props.nome}</h4>
+                <h5>{props.cargo}</h5>
+            </div>
         </div>
-        <div className='base'> 
-            <h4>{props.nome}</h4>
-            <h5>{props.cargo}</h5>
-        </div>
-    </div>
-    )
-
+    );
 }
 
-export default Colaborator
+export default Colaborator;
