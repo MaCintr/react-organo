9808d7b275880f3417a50e88eeed75fde54bebd0
MaCintr <marcelocintra2307@gmail.com>
2024-09-11 09:45:23 -0300

    Alterando dinamicamente o banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -4,7 +4,7 @@ import ThemeSelector from "../ThemeSelector"
 function Banner() {
     return (
         <header className="banner">
-        <img src="/images/BannerPurple.png" alt="Banner"/>
+        <img id="banner" src="/images/banner-roxo.png" alt="Banner"/>
         <ThemeSelector/>
         </header>
     )
--- src/components/ThemeSelector/index.js
+++ src/components/ThemeSelector/index.js
@@ -1,30 +1,39 @@
 import './ThemeSelector.css'
 
 const ThemeSelector = (props) => {
+    const html = document.querySelector('html')
+    const banner = document.getElementById('banner')
+
+    const alterarContexto = (contexto) => {
+        html.setAttribute('data-contexto', contexto)
+        html.setAttribute('banner-bg', contexto)
+        banner.setAttribute('src', `/images/banner-${contexto}.png`)
+        console.log('Tema alterado => ', contexto)
+    }
 
     const temas = [
         {
-            tema: 'Roxo',
+            tema: 'roxo',
             cor: '#a60eec'
         },
         {
-            tema: 'Azul',
+            tema: 'azul',
             cor: '#33fff7'
         },
         {
-            tema: 'Verde',
+            tema: 'verde',
             cor: '#33ff57'
         },
         {
-            tema: 'Vermelho',
+            tema: 'vermelho',
             cor: '#FF0000'
         },
         {
-            tema: 'Laranja',
+            tema: 'laranja',
             cor: '#ffa500'
         },
         {
-            tema: 'Claro',
+            tema: 'claro',
             cor: '#f5f5f5'
         },
     ]
@@ -34,7 +43,8 @@ const ThemeSelector = (props) => {
             <h4>Tema</h4>
             {temas.map(tema =>
                 <div className="quadrante-cor"
-                    key={tema.nome}
+                    key={tema.tema}
+                    onClick={() => alterarContexto(tema.tema)}
                     style={{
                         width: '30px',
                         height: '30px',
@@ -43,7 +53,7 @@ const ThemeSelector = (props) => {
                         border: 'none',
                         borderRadius: '5px',
                         margin: '5px'
-                      }}
+                    }}
                 />
             )}
         </div>
