8546adc7c75b9b20baf701330e6118e69ea4fe89
MaCintr <marcelocintra2307@gmail.com>
2024-09-13 11:37:17 -0300

    Correção na renderização excessiva do input e option

--- src/components/InputText/index.js
+++ src/components/InputText/index.js
@@ -6,55 +6,9 @@ const Input = (props) => {
   const onTypeText = (event) => {
     props.onChangeValue(event.target.value)
   }
-  const StyledWrapper = styled.div`
-    .formField {
-    margin: 10px;
-    position: relative;
-    margin: 50px;
-  }
-  
-  .formField input {
-    padding: 10px 15px;
-    outline: none;
-    border: none;
-    border-radius: 5px;
-    background-color: #ffffff;
-    color: #333;
-    font-size: 16px;
-    font-weight: 400;
-    transition: 0.3s ease-in-out;
-    box-shadow: 0 0 0 5px transparent;
-    width: 94.5%
-  }
-  
-  .formField input:hover,
-  .formField input:focus {
-    box-shadow: 0 0 0 2px ${props.temaState};
-  }
-  
-  .formField span {
-    position: absolute;
-    left: 0;
-    top: 0;
-    padding: 8px 15px;
-    color: #1a1d33;
-    font-size: 16px;
-    font-weight: 600;
-    transition: 0.3s ease-in-out;
-    pointer-events: none;
-  }
-  
-  .formField input:focus + span,
-  .formField input:valid + span {
-    transform: translateY(-32px) translateX(-5px) scale(0.95);
-    color: white;
-    transition: 0.3s ease-in-out;
-  }
-  
-  `;
 
   return (
-    <StyledWrapper>
+    <StyledWrapper temaState={props.temaState}>
       <div className="formField">
         <input value={props.valor} required type="text" onChange={onTypeText}/>
         <span>{props.label}</span>
@@ -62,6 +16,54 @@ const Input = (props) => {
     </StyledWrapper>
   );
 };
+
+const StyledWrapper = styled.div`
+.formField {
+margin: 10px;
+position: relative;
+margin: 50px;
+}
+
+.formField input {
+padding: 10px 15px;
+outline: none;
+border: none;
+border-radius: 5px;
+background-color: #ffffff;
+color: #333;
+font-size: 16px;
+font-weight: 400;
+transition: 0.3s ease-in-out;
+box-shadow: 0 0 0 5px transparent;
+width: 94.5%
+}
+
+.formField input:hover,
+.formField input:focus {
+box-shadow: 0 0 0 2px ${props => props.temaState};
+}
+
+.formField span {
+position: absolute;
+left: 0;
+top: 0;
+padding: 8px 15px;
+color: #1a1d33;
+font-size: 16px;
+font-weight: 600;
+transition: 0.3s ease-in-out;
+pointer-events: none;
+}
+
+.formField input:focus + span,
+.formField input:valid + span {
+transform: translateY(-32px) translateX(-5px) scale(0.95);
+color: white;
+transition: 0.3s ease-in-out;
+}
+
+`;
+
 
 
 export default Input;
--- src/components/Options/index.js
+++ src/components/Options/index.js
@@ -1,53 +1,54 @@
 import styled from "styled-components";
 
+const StyledWrapper = styled.div`
+  .formField {
+    margin: 10px;
+    position: relative;
+    margin: 50px;
+  }
+
+  .formField select {
+    padding: 10px 15px;
+    outline: none;
+    border: none;
+    border-radius: 5px;
+    background-color: #ffffff;
+    color: #333;
+    font-size: 16px;
+    font-weight: 500;
+    transition: 0.3s ease-in-out;
+    box-shadow: 0 0 0 5px transparent;
+    width: 100%;
+  }
+
+  .formField select:hover,
+  .formField select:focus {
+    box-shadow: 0 0 0 2px ${props => props.temaState};
+  }
+
+  .formField span {
+    position: absolute;
+    left: 0;
+    top: 0;
+    padding: 8px 15px;
+    color: #1a1d33;
+    font-size: 16px;
+    font-weight: 600;
+    transition: 0.3s ease-in-out;
+    pointer-events: none;
+  }
+
+  .formField select:focus + span,
+  .formField select:valid + span {
+    transform: translateY(-32px) translateX(-5px) scale(0.95);
+    color: white;
+    transition: 0.3s ease-in-out;
+  }
+`;
+
 const Options = (props) => {
-  const StyledWrapper = styled.div`
-    .formField {
-      margin: 10px;
-      position: relative;
-      margin: 50px;
-    }
-  
-    .formField select {
-      padding: 10px 15px;
-      outline: none;
-      border: none;
-      border-radius: 5px;
-      background-color: #ffffff;
-      color: #333;
-      font-size: 16px;
-      font-weight: 500;
-      transition: 0.3s ease-in-out;
-      box-shadow: 0 0 0 5px transparent;
-      width: 100%;
-    }
-  
-    .formField select:hover,
-    .formField select:focus {
-      box-shadow: 0 0 0 2px ${props.temaState};
-    }
-  
-    .formField span {
-      position: absolute;
-      left: 0;
-      top: 0;
-      padding: 8px 15px;
-      color: #1a1d33;
-      font-size: 16px;
-      font-weight: 600;
-      transition: 0.3s ease-in-out;
-      pointer-events: none;
-    }
-  
-    .formField select:focus + span,
-    .formField select:valid + span {
-      transform: translateY(-32px) translateX(-5px) scale(0.95);
-      color: white;
-      transition: 0.3s ease-in-out;
-    }
-  `;
   return (
-    <StyledWrapper>
+    <StyledWrapper temaState={props.temaState}>
       <div className="formField">
         <select required onChange={event => props.onChangeValue(event.target.value)} value={props.valor}>
           <option value=''></option>
@@ -62,6 +63,5 @@ const Options = (props) => {
     </StyledWrapper>
   );
 };
-
 
 export default Options;
