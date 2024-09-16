fe09de58c1df30d8a297454ce87f8226b978e0b5
MaCintr <marcelocintra2307@gmail.com>
2024-09-16 12:09:36 -0300

    FooterComponent - redirecionamento nos ícones

--- src/components/FooterComponent/index.js
+++ src/components/FooterComponent/index.js
@@ -12,10 +12,10 @@ const FooterComponent = (props) => {
             }}>
             <span>&copy; 2024 Marcelo Amarante Cintra.<br />Todos os direitos reservados.</span>
             <span className='socials'>Entre em contato<br />
-                <a href=''><i style={estiloIcone} className="bi bi-instagram"></i></a>
-                <a><i style={estiloIcone} className="bi bi-linkedin"></i></a>
-                <a><i style={estiloIcone} className="bi bi-github"></i></a>
-                <a><i style={estiloIcone} className="bi bi-envelope"></i></a>
+                <a href='https://www.instagram.com/marcelocintraa_/'><i style={estiloIcone} className="bi bi-instagram"></i></a>
+                <a href='https://www.linkedin.com/in/marcelo-cintra-64224b254/'><i style={estiloIcone} className="bi bi-linkedin"></i></a>
+                <a href='https://github.com/MaCintr'><i style={estiloIcone} className="bi bi-github"></i></a>
+                <a href='mailto:cintramarcelo2307@gmail.com'><i style={estiloIcone} className="bi bi-envelope"></i></a>
             </span>
         </footer>
     )
